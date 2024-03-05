9b4f2923477b0c37fafed3b48820e61f59bfb3fc
saharabanu <saharabanungn@gmail.com>
2024-03-05 13:16:40 +0600

    responsive navbar done

--- src/components/ui/ResponsiveNavbar.js
+++ src/components/ui/ResponsiveNavbar.js
@@ -1,49 +1,50 @@
-import logo from "../../assets/images/header-logo.png";
-import icon from "../../assets/images/people-icon.png";
+"use client";
+import { useState } from "react";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { GoSearch } from "react-icons/go";
-import { AiOutlineShoppingCart, AiOutlineHeart } from "react-icons/ai";
+import { AiOutlineHeart, AiOutlineShoppingCart } from "react-icons/ai";
 import Image from "next/image";
 import Link from "next/link";
-import ResponsiveNavbar from "./ResponsiveNavbar";
+import logo from "../../assets/images/header-logo.png";
+import icon from "../../assets/images/people-icon.png";
+import { MdMenu } from "react-icons/md";
 
-const Navbar = () => {
+const ResponsiveNavbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <header>
       <div className="container">
-        <div className=" lg:px-20 px-5">
-          <div className="hidden lg:grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-1 sm:c gap-4 items-center">
-            <div>
-              <ul className="flex items-center space-x-6">
-                <li className="">
-                  <Link href="/">Home</Link>
-                </li>
-                <li>
-                  <Link href="/" className="flex justify-center items-center">
-                    Pages{" "}
-                    <span>
-                      <MdKeyboardArrowDown />
-                    </span>
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/" className="flex justify-center items-center">
-                    Menu{" "}
-                    <span>
-                      <MdKeyboardArrowDown />
-                    </span>
-                  </Link>
-                </li>
-                <li>
-                  <Link href="/">Contact Us</Link>
-                </li>
-              </ul>
-            </div>
+        <div className="lg:px-20 px-5 py-5">
+          <div className="grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-1 sm:c gap-4 items-center">
+            <div className="flex justify-between items-center lg:hidden">
+              <Image src={logo} width={150} height={50} alt="Logo" />
 
-            <div className="flex">
-              <Image src={logo} width={150} height={10} alt="FooterLogo" />
+              <span
+                className="cursor-pointer"
+                onClick={() => setShowMenu(!showMenu)}
+              >
+                <MdMenu />
+              </span>
             </div>
-            <div className=" flex justify-between items-center">
+          </div>
+          {showMenu && (<>
+            <ul className="lg:hidden flex flex-col items-center mt-4 space-y-2">
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/">Pages</Link>
+              </li>
+              <li>
+                <Link href="/">Menu</Link>
+              </li>
+              <li>
+                <Link href="/">Contact Us</Link>
+              </li>
+            </ul>
+            <div className="flex justify-center items-center">
+            <div className=" ">
               <ul className="flex items-center space-x-2">
                 <li className="border rounded-full border-white flex justify-center items-center p-2 transition-all duration-300 delay-300 hover:text-red-200">
                   <Link href="/" className="">
@@ -90,12 +91,13 @@ const Navbar = () => {
                 </li>
               </ul>
             </div>
-          </div>
+            </div>
+            </>
+          )}
         </div>
       </div>
-      <ResponsiveNavbar />
     </header>
   );
 };
 
-export default Navbar;
+export default ResponsiveNavbar;
